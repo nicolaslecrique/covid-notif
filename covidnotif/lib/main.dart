6bf20f3d0de2182b2edf99942f9d46905d8f0839
Mokhtari Mohamed <mohamed.mokhtari2@gmail.com>
2020-04-01 20:20:38 +0200

    First draft of the UX, - Home route with 2 buttons (Contacts & Sick/Curred buttons) - Contacts route with a list of contacts

--- covidnotif/lib/main.dart
+++ covidnotif/lib/main.dart
@@ -1,111 +1,126 @@
+import "dart:math";
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Covid-19 Notifier',
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  HomeState createState() => HomeState(false);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomeState extends State<Home> {
+  bool _sick;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  HomeState(this._sick);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var healthText = _sick
+        ? Text(
+            "I'm sick",
+            style: TextStyle(fontSize: 24, backgroundColor: Colors.red),
+          )
+        : Text(
+            "I'm curred",
+            style: TextStyle(fontSize: 24, backgroundColor: Colors.green),
+          );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("I'm ${!_sick ? 'sick' : 'curred'}"),
+      content: Text("Do you confirm you are ${!_sick ? 'sick' : 'curred'} ?"),
+      actions: [
+        FlatButton(
+          child: Text("Cancel"),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: Text("Confirm"),
+          onPressed: () {
+            setState(() {
+              _sick = !_sick;
+            });
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        title: Center(
+          child: Text('Covid-19 Notifier'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.contacts),
+            title: Text(
+              'Contacts',
+              style: TextStyle(fontSize: 24),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ContactsRoute()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.local_hospital),
+            title: healthText,
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return alert;
+                },
+              );
+            },
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class ContactsRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cities = ['La Défense', 'Paris', 'Courbevoie', 'Marseille', 'Orléan'];
+    var now = new DateTime.now();
+
+    var contacts = cities.map((city) {
+      return Text(
+        '${DateFormat('y, MMM, d H:m').format(now)} at $city',
+        style: TextStyle(fontSize: 24),
+      );
+    }).toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('Contacts'),
+          ),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: contacts,
+        ));
   }
 }
